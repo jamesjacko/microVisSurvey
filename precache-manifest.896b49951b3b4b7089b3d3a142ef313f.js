f6c5c91be244f4b0f14ce63b7ad424cfa25c1353
Unknown <james.ogge@gmail.com>
2019-02-21 11:57:33 +0000

    Initial commit

---
+++ precache-manifest.896b49951b3b4b7089b3d3a142ef313f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b155f3691eab0bf20906861c072edcf4",
+    "url": "/microVisSurvey/static/media/Bangor_Logo_A1.b155f369.png"
+  },
+  {
+    "revision": "b478e3108b6c6446f32a",
+    "url": "/microVisSurvey/static/js/runtime~main.b478e310.js"
+  },
+  {
+    "revision": "f3dc880ecaa3fa57f1cc",
+    "url": "/microVisSurvey/static/js/main.f3dc880e.chunk.js"
+  },
+  {
+    "revision": "a0a0e1040aa421eb4ea8",
+    "url": "/microVisSurvey/static/js/1.a0a0e104.chunk.js"
+  },
+  {
+    "revision": "f3dc880ecaa3fa57f1cc",
+    "url": "/microVisSurvey/static/css/main.4de1465b.chunk.css"
+  },
+  {
+    "revision": "77dc0a20c9418e11a347f60ad2adfe2b",
+    "url": "/microVisSurvey/index.html"
+  }
+];
